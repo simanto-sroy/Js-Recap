8a6881f6f86ba853d463a8f66085193ed19d28a4
Simanto Roy <sroy38703@gmail.com>
2021-12-23 12:33:05 +0600

    recap-js-hosting

---
+++ chapter_three/hosting.js
@@ -0,0 +1,40 @@
+// Hosting
+
+var a = 100;
+
+// newPrint(a);
+
+print(20)
+
+var newPrint = print;
+newPrint(45)
+
+function print(a) {
+    console.log (a)
+}
+
+print(a)
+
+
+/**
+ * Creational Phase
+ */
+
+/*
+    1. variable a = undefined
+    2. ignore callin function
+    3. function = ..referacne
+
+*/
+
+
+/**
+ * Execution Phase
+ */
+
+/*
+    1. print value 20
+    2. print value 45
+    3. print value 100
+
+*/
