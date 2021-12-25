2bb6156d402302fe718f2907a039daba79b397dd
Simanto Roy <sroy38703@gmail.com>
2021-12-26 05:45:59 +0600

    inheritance-and-composition-mixin-es5

--- chapter_seven/app.js
+++ chapter_seven/app.js
@@ -1,38 +1,82 @@
-// Prototipycal Inheritance
+// // Prototipycal Inheritance
 
-function extend(Parent, Child) {
-    Child.prototype = Object.create(Parent.prototype);
-    Child.prototype.constructor = Child
+// function extend(Parent, Child) {
+//     Child.prototype = Object.create(Parent.prototype);
+//     Child.prototype.constructor = Child
+// }
+
+// function Shape(color) {
+//     this.color = color
+// }
+
+// Shape.prototype.common = function () {
+//     console.log('I am Common Method')
+// }
+
+// const Rect = function (width, color) {
+//     Shape.call(this, color)
+//     this.width = width
+// }
+// extend(Shape, Rect)
+
+// Rect.prototype.draw = function () {
+//     console.log('I am Draw Method')
+// }
+
+// const Circel = function (height) {
+//     this.height = height
+// }
+// extend(Shape, Circel)
+
+// Circel.prototype.common = function () {
+//     Shape.prototype.common.call(this)
+//     console.log('I am Calling from common and I am overreaden')
+// }
+
+// // let shape = new Shape()
+// let rect = new Rect(32, 'red')
+// let circel = new Circel(23)
+
+
+
+// Inheritance and Compositon Mixin
+
+function mixin(target, ...sources) {
+    Object.assign(target, ...sources)
 }
 
-function Shape(color) {
-    this.color = color
+var canWalk = {
+    walk: function() {
+        console.log('Walking...')
+    }
 }
 
-Shape.prototype.common = function () {
-    console.log('I am Common Method')
+var canEat = {
+    Eat: function () {
+        console.log('Eatting...')
+    }
 }
 
-const Rect = function (width, color) {
-    Shape.call(this, color)
-    this.width = width
-}
-extend(Shape, Rect)
-
-Rect.prototype.draw = function () {
-    console.log('I am Draw Method')
+var canSwim = {
+    Swim: function () {
+        console.log('Swiping...')
+    }
 }
 
-const Circel = function (height) {
-    this.height = height
-}
-extend(Shape, Circel)
-
-Circel.prototype.common = function () {
-    Shape.prototype.common.call(this)
-    console.log('I am Calling from common and I am overreaden')
+function Person(name) {
+    this.name = name
 }
 
-// let shape = new Shape()
-let rect = new Rect(32, 'red')
-let circel = new Circel(23)
+mixin(Person.prototype, canWalk, canEat);
+
+var personOne = new Person('Simanto Roy')
+console.log(personOne)
+
+function GoldFish(name) {
+    this.name = name
+}
+
+mixin(GoldFish.prototype, canSwim, canEat)
+
+var goldFishOne = new GoldFish('GoldFish')
+console.log(goldFishOne)
